112131a786774d9f6cdbdafc0df439bb77a8e1d3
MD. JAHIDUL ISLAM SUJAN <31595886+JAHIDofficial@users.noreply.github.com>
2020-06-26 22:54:58 +0600

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+main.js
