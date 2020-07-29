79ffc221e984d46257eaf8987bc6343dec45e158
MD. JAHIDUL ISLAM SUJAN <31595886+JAHIDofficial@users.noreply.github.com>
2020-07-30 01:13:22 +0600

    v3commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// Toggle the menu bar when it's clicked
+function openNav() {
+	document.getElementById("navbar").style.height = "auto";
+}
+
+function closeNav() {
+	document.getElementById("navbar").style.height = "0%";
+}
+
+// Close the menu when the menu links are clicked
+document.querySelector(".welcomeMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".aboutMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".educationsMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".skillsMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".certificationsMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".workMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".projectsMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
+
+document.querySelector(".contactMenu").addEventListener("click", function () {
+	document.getElementById("navbar").style.height = "0%";
+});
