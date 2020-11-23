18c76b7202f4f43e721ef39502605168798f2d0f
MD. JAHIDUL ISLAM SUJAN <jahid.profile@gmail.com>
2020-11-24 01:13:51 +0600

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Toggle the menu bar when it's clicked
 
 function openNav() {
-	document.querySelector("#overlay").style.height = "auto";
+    document.querySelector("#overlay").style.height = "auto";
 }
 
 function closeNav() {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 }
 
 // Close the menu when the menu links are clicked
@@ -15,25 +15,25 @@ function closeNav() {
 //});
 
 document.querySelector(".aboutMenu").addEventListener("click", function () {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 });
 
 document.querySelector(".studyMenu").addEventListener("click", function () {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 });
 
 document.querySelector(".workMenu").addEventListener("click", function () {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 });
 
 document.querySelector(".skillsMenu").addEventListener("click", function () {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 });
 
 document.querySelector(".portfolioMenu").addEventListener("click", function () {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 });
 
 document.querySelector(".contactMenu").addEventListener("click", function () {
-	document.querySelector("#overlay").style.height = "0%";
+    document.querySelector("#overlay").style.height = "0%";
 });
